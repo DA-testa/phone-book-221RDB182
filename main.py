6937d70c7a3d8c12e6d5879a6524e82ed60f7b9f
Aleksandrs K <123946459+221RDB182@users.noreply.github.com>
2023-04-03 15:21:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,52 @@
 # python3
 
 import sys
-
 class Contact:
     def __init__(self, name, number):
         self.name = name
         self.number = number
 
+class Query:
+    def __init__(self, query):
+        self.type = query[0]
+        self.number = int(query[1])
+        if self.type == 'add':
+            self.name = query[2]
+
 def read_queries():
     n = int(input())
-    return [input().split() for i in range(n)]
+    return [Query(input().split()) for i in range(n)]
 
 def write_responses(result):
-    with sys.stdout as output:
-        output.write('\n'.join(result))
+    print('\n'.join(result))
 
 def process_queries(queries):
     result = []
-    contacts = {}
-    for query in queries:
-        query_type = query[0]
-        if query_type == 'add':
-            name, number = query[1], query[2]
-            if not number.isdigit():
-                result.append('Invalid phone number')
-            else:
-                contacts[number] = Contact(name, number)
-        elif query_type == 'del':
-            number = query[1]
-            if not number.isdigit():
-                result.append('Invalid phone number')
-            elif number in contacts:
-                contacts.pop(number)
-            else:
-                result.append('Phone number not found')
-        elif query_type == 'find':
-            number = query[1]
-            if not number.isdigit():
-                result.append('Invalid phone number')
-            elif number in contacts:
-                result.append(contacts[number].name)
-            else:
-                result.append('Phone number not found')
+    # Keep list of all existing (i.e. not deleted yet) contacts.
+    contacts = []
+    for cur_query in queries:
+        if cur_query.type == 'add':
+            # if we already have contact with such number,
+            # we should rewrite contact's name
+            for contact in contacts:
+                if contact.number == cur_query.number:
+                    contact.name = cur_query.name
+                    break
+            else: # otherwise, just add it
+                contacts.append(Contact(cur_query.name, cur_query.number))
+        elif cur_query.type == 'del':
+            for j in range(len(contacts)):
+                if contacts[j].number == cur_query.number:
+                    contacts.pop(j)
+                    break
         else:
-            result.append('Invalid query')
+            response = 'not found'
+            for contact in contacts:
+                if contact.number == cur_query.number:
+                    response = contact.name
+                    break
+            result.append(response)
     return result
 
 if __name__ == '__main__':
-    queries = read_queries()
-    result = process_queries(queries)
-    write_responses(result)
-    
+    write_responses(process_queries(read_queries()))
